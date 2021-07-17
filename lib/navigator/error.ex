9a223aea80b0233f213db7aee051a50ddf3db0d9
Eduardo <eduardofbaptista@gmail.com>
2021-07-17 17:36:54 -0300

    Add function to create error struct to a resource not found

--- lib/navigator/error.ex
+++ lib/navigator/error.ex
@@ -33,4 +33,12 @@ defmodule Navigator.Error do
   def build_with_changeset(%Changeset{} = changeset) do
     build(:bad_request, changeset)
   end
+
+  @spec build_resource_not_found(binary()) :: t()
+  @doc """
+  Create new Error struct to a resource not found
+  """
+  def build_resource_not_found(resource) when is_binary(resource) do
+    build(:not_found, "#{resource} not found")
+  end
 end
