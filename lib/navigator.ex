2a528cc6a15d89462a426ca82d249092a0cd9109
Eduardo <eduardofbaptista@gmail.com>
2021-07-18 00:16:28 -0300

    Add navigator delegates to expose functions

--- lib/navigator.ex
+++ lib/navigator.ex
@@ -6,4 +6,8 @@ defmodule Navigator do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  defdelegate create_probe(params), to: Navigator.Probes.Create, as: :call
+  defdelegate get_probe_by_id(id), to: Navigator.Probes.Get, as: :by_id
+  defdelegate move_probe(id, movements), to: Navigator.Probes.Move, as: :call
+  defdelegate reset_probe_position(id), to: Navigator.Probes.Reset, as: :call
 end
